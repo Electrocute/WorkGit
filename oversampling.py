0d18ed9ee42f32098c76bbb8f390296b275df6d9
Electrocute <localgoddess@gmail.com>
2015-04-07 14:54:04 -0400

    Bit manipulation first try
    Trying to use bitstring

--- oversampling.py
+++ oversampling.py
@@ -1,13 +1,30 @@
 import filecmp
+import os
+from bitstring import BitArray, BitStream
 
 original_file = input("Enter the file you wish to over sample: ");
 print("the file you want to oversample is:", original_file);
 final_product = input("Enter the oversampled file: ");
 print("The oversampled product of ", original_file, " is stored in ", final_product);
+int n 
+n = input("Rate of oversampling: ")
 
+old_file = open(original_file, 'rb');
+new_file = open(final_product, 'wb');
 
-old_file = open(original_file, 'r');
+file_statistics = os.stat('old_file')
+file_length = file_statistics.st_size
+file_length_binary = file_length * 7
 
-new_file = open(final_product, 'w');
-
-old_file.read()
+old_file_binary = Bits(old_file)
+new_file_binary = Bits(new_file)
+i=0
+bool temp
+while(j<file_length_binary)
+	temp = old_file_binary.read(j) 
+	if temp = 1
+		for(k=0, k<n, k++)
+			new_file_binary.append(0b1)
+	else
+		for(k=0, k<n, k++)
+			new_file_binary.append(0b0)
