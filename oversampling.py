abf375a834b86fca02bb944d7bf1ee3f9595448f
Electrocute <localgoddess@gmail.com>
2015-05-01 17:29:07 -0400

    working
    for multiples of 8

--- oversampling.py
+++ oversampling.py
@@ -9,7 +9,7 @@ print("the file you want to oversample is:", original_file);
 final_product = input("Enter the oversampled file: ");
 print("The oversampled product of ", original_file, " is stored in ", final_product);
 #file_size = input( "Enter the size of the file in bytes: ")
-n = 0
+
 n = input("Rate of oversampling: ")
 
 
@@ -31,59 +31,76 @@ value = 0
 #old_file = open(original_file, 'rb');
 new_file = open(final_product, 'wb');
 
-def writeBits(number):
-    
+def writeBits(number, write_value):
+    print("number to write: ", number, "with value of: ", write_value)
     bytes_to_write = number/8
     remainder = number%8
-    if bytes_to_write>1:
-        i = 1
+    print("remainder is: ", remainder)
+    bytes_to_write = int(bytes_to_write)
+    print("Bytes to write:  ", bytes_to_write)
+    if bytes_to_write>0:
         for i in range(bytes_to_write):
-            if value == 1:
-                new_file.write(0xFF)
+            print("inside for loop in writeBits")
+            if write_value == 0:
+                new_file.write(b'\x00')
+                print("writing 00")
             else:
-                new_file.write(0x00)
-            return
+                new_file.write(b'\xFF')
+                print("writing FF")
+        return(remainder)
 
 
-def changeValue():
+def changeValue(newValue, remainder):
+    print("changing value")
+    print("the remainder is: ", remainder)
+    if(remainder == 0):
+        value = newValue
+        print("changed value to", value)
+        writeBits(int(n), int(value))
+        return
     if remainder == 1:
         if newValue ==0 :
-            new_file.write(change_1^0x0)
+            new_file.write(change_1^0x00)
         else:
-            new_file.write(change_1^0xF)
+            new_file.write(change_1^0xFF)
     elif remainder == 2:
         if newValue ==0 :
-            new_file.write(change_2^0x0)
+            new_file.write(change_2^0x00)
         else:
-            new_file.write(change_20xF)
+            new_file.write(change_2^0xFF)
     elif remainder ==3:
         if newValue ==0 :
-            new_file.write(change_3^0x0)
+            new_file.write(change_3^0x00)
         else:
-            new_file.write(change_3^0xF)
+            new_file.write(change_3^0xFF)
     elif remainder == 4:
         if newValue ==0 :
-            new_file.write(change_4^0x0)
+            new_file.write(change_4^0x00)
         else:
-            new_file.write(change_4^0xF)
+            new_file.write(change_4^0xFF)
     elif remainder ==5:
         if newValue ==0 :
-            new_file.write(change_5^0x0)
+            new_file.write(change_5^0x00)
+            print(change_5^0x00, "is the change byte")
         else:
-            new_file.write(change_5^0xF)
+            new_file.write(change_5^0xFF)
+            print(change_5^0xFF, "is the change byte")
     elif remainder == 6:
         if newValue ==0 :
-            new_file.write(change_6^0x0)
+            new_file.write(change_6^0x00)
         else:
-            new_file.write(change_6^0xF)
+            new_file.write(change_6^0xFF)
     elif remainder ==7:
         if newValue ==0 :
-            new_file.write(change_7^0x0)
+            new_file.write(change_7^0x00)
         else:
-            new_file.write(change_7^0xF)
+            new_file.write(change_7^0xFF)
     
     value = newValue
-    writeBits(n-(4-remainder))
+    print("changed value to", value)
+    to_write = int(n)-(8-int(remainder))
+    remainder = writeBits(to_write)
+    return(remainder)
 
 #file_length_binary = file_size * 8
 
@@ -91,25 +108,66 @@ mask_1 = 0x80  # 0b1000
 mask_2 = 0x40  # 0b0100
 mask_3 = 0x20  # 0b0010 
 mask_4 = 0x10  # 0b0001 
-remainder = 0
-value = 0
+mask_5 = 0x08  # 0b1000
+mask_6 = 0x04  # 0b0100
+mask_7 = 0x02  # 0b0010 
+mask_8 = 0x01  # 0b0001 
+
+newValue = 0
+
+def processByte(new_byte, remainder):
+    if (new_byte > 0 ):
+        newValue = 1
+    else :
+        newValue = 0 
+    if newValue == value : 
+        temp_remainder = remainder
+        number_to_write = int(n) + int(temp_remainder)
+        remainder = writeBits(number_to_write, value)
+    else:
+        remainder = changeValue(newValue, remainder)
+    return(remainder)
+
+number_to_write = 0
+
 with open(original_file, "rb") as f:
     byte = f.read(1)
     while byte:
         print("Byte as read: ", byte)
         #new_byte = struct.unpack(byte)
         print("Byte unpacked: ", byte[0])
+
         new_byte = byte[0] & mask_1
         print("after mask: ", new_byte)
-        if (new_byte > 0 ):
-            newValue = 1
-        else :
-            newValue = 0
-        if newValue == value : 
-            number_to_write = n+remainder
-            writeBits(number_to_write)
-        else:
-            changeValue()
+        remainder = processByte(new_byte, remainder)
+
+        new_byte = byte[0] & mask_2
+        print("after mask: ", new_byte)
+        remainder = processByte(new_byte, remainder)
+
+        new_byte = byte[0] & mask_3
+        print("after mask: ", new_byte)
+        remainder = processByte(new_byte, remainder)
+
+        new_byte = byte[0] & mask_4
+        print("after mask: ", new_byte)
+        remainder = processByte(new_byte, remainder)
+
+        new_byte = byte[0] & mask_5
+        print("after mask: ", new_byte)
+        remainder = processByte(new_byte, remainder)
+
+        new_byte = byte[0] & mask_6
+        print("after mask: ", new_byte)
+        remainder = processByte(new_byte, remainder)
+
+        new_byte = byte[0] & mask_7
+        print("after mask: ", new_byte)
+        remainder = processByte(new_byte, remainder)
+
+        new_byte = byte[0] & mask_8
+        print("after mask: ", new_byte)
+        remainder = processByte(new_byte, remainder)
 
         # Do stuff with byte.
         byte = f.read(1)
